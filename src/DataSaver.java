f70b65e377ddb4f550750b3474819cfe6f43b025
jainks-1 <jainks@mail.uc.edu>
2025-04-13 18:31:59 -0400

    Part 2 done, lab complete

--- src/DataSaver.java
+++ src/DataSaver.java
@@ -4,67 +4,53 @@ import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.nio.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import static java.nio.file.StandardOpenOption.*;
+import static java.nio.file.StandardOpenOption.CREATE;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class DataSaver
-{
+public class DataSaver {
 
-    public static void main(String[] args)
-    {
-        // Test data the lines of the file we will write
-        ArrayList <String>recs = new ArrayList<>();
-        recs.add("Sample data for our file writing example.");
-        recs.add("Sample data Line 2.");
-        recs.add("Sample data Line 3.");
-        recs.add("Sample data Line 4.");
-        recs.add("Sample data Line 5.");
+    public static void main(String[] args) {
+        ArrayList<String> recs = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+        boolean done = false;
+        int idCounter = 1; // Start ID counter at 1
 
+        do {
+            String firstName = SafeInput.getNonZeroLenString(in, "Enter first name");
+            String lastName = SafeInput.getNonZeroLenString(in, "Enter last name");
+            String id = String.format("%06d", idCounter); // Format ID with leading zeros
+            String email = SafeInput.getNonZeroLenString(in, "Enter email");
+            int yearOfBirth = SafeInput.getRangedInt(in, "Enter year of birth", 1900, 2024); // Adjust range as needed
 
-        // uses a fixed known path:
-        //  Path file = Paths.get("c:\\My Documents\\data.txt");
+            String record = firstName + ", " + lastName + ", " + id + ", " + email + ", " + yearOfBirth;
+            recs.add(record);
 
-        // use the toolkit to get the current working directory of the IDE
-        // will create the file within the Netbeans project src folder
-        // (may need to adjust for other IDE)
-        // Not sure if the toolkit is thread safe...
+            idCounter++; // Increment ID counter
+
+            done = SafeInput.getYNConfirm(in, "Add another record");
+        } while (done);
+
+        String fileName = "data.csv";
         File workingDirectory = new File(System.getProperty("user.dir"));
-        Path file = Paths.get(workingDirectory.getPath() + "\\src\\data.txt");
+        Path file = Paths.get(workingDirectory.getPath() + "/src/" + fileName); // Save to src directory
 
-        /** This down will be copied intot the program.
-         the loop will be created via a loop */
+        try {
+            OutputStream out = new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
 
-        try
-        {
-            // Typical java pattern of inherited classes
-            // we wrap a BufferedWriter around a lower level BufferedOutputStream
-            OutputStream out =
-                    new BufferedOutputStream(Files.newOutputStream(file, CREATE));
-            BufferedWriter writer =
-                    new BufferedWriter(new OutputStreamWriter(out));
-
-            // Finally can write the file LOL!
-
-            for(String rec : recs)
-            {
-                writer.write(rec, 0, rec.length());  // stupid syntax for write rec
-                // 0 is where to start (1st char) the write
-                // rec. length() is how many chars to write (all)
-                writer.newLine();  // adds the new line
-
+            for (String rec : recs) {
+                writer.write(rec);
+                writer.newLine();
             }
-            writer.close(); // must close the file to seal it and flush buffer
-            System.out.println("Data file written!");
-        }
-        catch (IOException e)
-        {
+
+            writer.close();
+            System.out.println("Data written to " + fileName);
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-
 }
