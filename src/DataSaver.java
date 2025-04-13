1844117e1f03fccb15d4546d73a72c157908d93a
jainks-1 <jainks@mail.uc.edu>
2025-04-13 18:15:28 -0400

    Part 1 of lab done

--- src/DataSaver.java
+++ src/DataSaver.java
@@ -1,4 +1,70 @@
-package PACKAGE_NAME;
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static java.nio.file.StandardOpenOption.*;
+import java.util.ArrayList;
 
-public class DataSaver {
+public class DataSaver
+{
+
+    public static void main(String[] args)
+    {
+        // Test data the lines of the file we will write
+        ArrayList <String>recs = new ArrayList<>();
+        recs.add("Sample data for our file writing example.");
+        recs.add("Sample data Line 2.");
+        recs.add("Sample data Line 3.");
+        recs.add("Sample data Line 4.");
+        recs.add("Sample data Line 5.");
+
+
+        // uses a fixed known path:
+        //  Path file = Paths.get("c:\\My Documents\\data.txt");
+
+        // use the toolkit to get the current working directory of the IDE
+        // will create the file within the Netbeans project src folder
+        // (may need to adjust for other IDE)
+        // Not sure if the toolkit is thread safe...
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\data.txt");
+
+        /** This down will be copied intot the program.
+         the loop will be created via a loop */
+
+        try
+        {
+            // Typical java pattern of inherited classes
+            // we wrap a BufferedWriter around a lower level BufferedOutputStream
+            OutputStream out =
+                    new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer =
+                    new BufferedWriter(new OutputStreamWriter(out));
+
+            // Finally can write the file LOL!
+
+            for(String rec : recs)
+            {
+                writer.write(rec, 0, rec.length());  // stupid syntax for write rec
+                // 0 is where to start (1st char) the write
+                // rec. length() is how many chars to write (all)
+                writer.newLine();  // adds the new line
+
+            }
+            writer.close(); // must close the file to seal it and flush buffer
+            System.out.println("Data file written!");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- src/FileInspector.java
+++ src/FileInspector.java
@@ -1,15 +1,66 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class FileInspector {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import static java.lang.System.out;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import javax.swing.JFileChooser;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class FileInspector {
+
+    public static void main(String[] args) {
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+
+        try {
+            File workingDirectory = new File(System.getProperty("user.dir") + "/src"); // Corrected path
+
+            chooser.setCurrentDirectory(workingDirectory);
+
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+
+                InputStream in = new BufferedInputStream(Files.newInputStream(file)); // Removed CREATE
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                int lineCount = 0;
+                int wordCount = 0;
+                int charCount = 0;
+
+                while ((rec = reader.readLine()) != null) { // Changed reader.ready() to readLine() != null
+                    lineCount++;
+                    charCount += rec.length();
+                    String[] words = rec.split("\\s+");
+                    wordCount += words.length;
+
+                    System.out.printf("\nLine %4d %-60s ", lineCount, rec); // Echo each line
+                }
+
+                System.out.println("\n\n--- Summary Report ---");
+                System.out.println("File: " + selectedFile.getName());
+                System.out.println("Lines: " + lineCount);
+                System.out.println("Words: " + wordCount);
+                System.out.println("Characters: " + charCount);
+
+                reader.close();
+                System.out.println("\n\nData file read!");
+            } else {
+                System.out.println("Failed to choose a file to process");
+                System.out.println("Run the program again!");
+                System.exit(0);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!!!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
